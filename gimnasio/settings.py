92144bcdfa39a3a661494ad43e770df4e0ad452c
libardo2s <libardoii@hotmail.com>
2015-02-24 12:54:21 -0500

    collectstatic

--- gimnasio/settings.py
+++ gimnasio/settings.py
@@ -90,7 +90,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'appgimnasio/template'),)
