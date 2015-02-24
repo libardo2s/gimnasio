f852ab5e0b8ec7f6792fd48c07c3b02e440758fd
libardo2s <libardoii@hotmail.com>
2015-02-24 12:56:12 -0500

    collectstatic

--- serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from appgym.models import Gimnasio
-
-class GimnasioSerializers(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Gimnasio
-		fields = ('nombre','direccion','telefono','correo','urlfacebook','urltwitter',)
